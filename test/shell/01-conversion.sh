8078928c9c6ea87f9baf74beb3a36e81e45271b6
raju <gururaju4240@gmail.com>
2026-06-01 12:46:12 +0530

    expense

---
+++ test/shell/01-conversion.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+echo "suresh:: hello ramesh, how are you?"
+echo "ramesh:: Hi suresh, iam fine thanks for asking. how are doing?"
+echo "suresh:: doing fine. what are you learing ramesh?"
+echo "ramesh:: iam are learning shell. what about you?"
