da75df9a15adbbe70535a01d018c6b555e5ee954
raju <gururaju4240@gmail.com>
2026-06-01 18:00:37 +0530

    expense

--- test/shell/05-var.sh
+++ test/shell/05-var.sh
@@ -7,5 +7,5 @@ echo "script executed at : $START_TIME"
 sleep 10
 
 END_TIME=$(date +%s)
-TOTAL_TIME=$(($SEND_TIME-$START_TIME))
+TOTAL_TIME=$(($END_TIME-$START_TIME))
 echo "script executed in: $TOTAL_TIME"
