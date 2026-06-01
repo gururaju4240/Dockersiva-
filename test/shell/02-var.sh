cf3eacacb00d63acd86a6e092356c6e7fe372418
raju <gururaju4240@gmail.com>
2026-06-01 13:08:44 +0530

    expense

--- test/shell/02-var.sh
+++ test/shell/02-var.sh
@@ -5,5 +5,5 @@ PERSON2=RAMESH
 
 echo "$PERSON1:: hello $PERSON2, how are you?"
 echo "PERSON2:: Hi $PERSON1, iam fine thanks for asking. how are doing?"
-echo "$PERSON1:: doing fine. what are you learing PERSON2?"
-echo "PERSON2:: iam are learning shell. what about you?"
+echo "$PERSON1:: doing fine. what are you learing $PERSON2?"
+echo "$PERSON2:: iam are learning shell. what about you?"
