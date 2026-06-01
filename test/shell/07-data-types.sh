aed4628a3f29325d1667c42de5f72e248cb38b51
raju <gururaju4240@gmail.com>
2026-06-01 18:47:44 +0530

    expense

--- test/shell/07-data-types.sh
+++ test/shell/07-data-types.sh
@@ -8,7 +8,8 @@ echo "SUM is $SUM"
 
 #array 
 FRUITS=("apple","banana","pomo")
+
 echo "Fruits are: ${FRUITS[@]}"
-echo "first fruit is :${FRUITS[0]}"
-echo "second fruit is :${FRUITS[1]}"
-echo "third fruit is :${FRUITS[2]}"
+echo "first fruit is: ${FRUITS[0]}"
+echo "second fruit is: ${FRUITS[1]}"
+echo "third fruit is: ${FRUITS[2]}"
