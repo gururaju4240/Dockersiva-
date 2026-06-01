49339602f25e7ee30e449588881ef88b957231f5
raju <gururaju4240@gmail.com>
2026-06-01 18:45:32 +0530

    expense

--- test/shell/07-data-types.sh
+++ test/shell/07-data-types.sh
@@ -5,3 +5,10 @@ NUM2=200
 
 SUM=$(($NUM1+$NUM2))
 echo "SUM is $SUM"
+
+#array 
+FRUITS=("apple","banana","pomo")
+echo "Fruits are: ${FRUITS[@]}"
+echo "first fruit is :${FRUITS[0]}"
+echo "second fruit is :${FRUITS[1]}"
+echo "third fruit is :${FRUITS[2]}"
