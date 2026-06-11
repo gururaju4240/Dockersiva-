7536f5e937a1992e2f6cd89218c25324462829a0
raju <gururaju4240@gmail.com>
2026-06-11 17:47:32 +0530

    expense

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -17,15 +17,15 @@ resource "aws_security_group" "allow_all" {
   }
 }
 
-resource "aws_instance" "ec2" {
+resource "aws_instance" "ec1" {
   ami           = "ami-00e801948462f718a"
   instance_type = "t3.micro"
 
   vpc_security_group_ids = [aws_security_group.allow_all.id]
 
   tags = {
-    Name    = "Hello"
-    Project = "roboshop"
+    Name    = "Hello1"
+    Project = "roboshop1"
   }
 }
 
