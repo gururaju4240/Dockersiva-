c8ef047829f8601afdd1c2fca7847518ab46e1d9
raju <gururaju4240@gmail.com>
2026-06-11 17:51:25 +0530

    expense

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,3 +1,14 @@
+resource "aws_instance" "ec1" {
+  ami           = "ami-00e801948462f718a"
+  instance_type = "t3.micro"
+
+  vpc_security_group_ids = [aws_security_group.allow_all.id]
+
+  tags = {
+    Name    = "Hello1"
+    Project = "roboshop1"
+  }
+}
 resource "aws_security_group" "allow_all" {
   name        = "allow-all-terraform"
   description = "Allow all inbound and outbound traffic"
@@ -17,16 +28,6 @@ resource "aws_security_group" "allow_all" {
   }
 }
 
-resource "aws_instance" "ec1" {
-  ami           = "ami-00e801948462f718a"
-  instance_type = "t3.micro"
 
-  vpc_security_group_ids = [aws_security_group.allow_all.id]
-
-  tags = {
-    Name    = "Hello1"
-    Project = "roboshop1"
-  }
-}
 
 
