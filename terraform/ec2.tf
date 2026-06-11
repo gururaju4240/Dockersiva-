b5abb2f27081e2567f252b11dc1d7ca91fb0ce6c
raju <gururaju4240@gmail.com>
2026-06-11 17:43:23 +0530

    expense

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,33 +1,32 @@
+resource "aws_security_group" "allow_all" {
+  name        = "allow-all-terraform"
+  description = "Allow all inbound and outbound traffic"
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "example" {
   ami           = "ami-00e801948462f718a"
   instance_type = "t3.micro"
 
-  tags = {
-    Name = "Hello"
-    project = "roboshop"
-  }
-}
+  vpc_security_group_ids = [aws_security_group.allow_all.id]
 
-  resource "aws_security_group" "allow_tls" {
-  name        = "allow -all-terraform"  # this aws account
-  description = "Allow TLS inbound traffic and all outbound traffic"
-   
-   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+  tags = {
+    Name    = "Hello"
+    Project = "roboshop"
   }
-   
-   ingress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-  
 }
 
 
