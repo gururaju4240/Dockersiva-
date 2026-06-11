8330ec11fed01e3f0c2c8794eabe0913607dd2a5
raju <gururaju4240@gmail.com>
2026-06-11 17:45:32 +0530

    expense

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -17,7 +17,7 @@ resource "aws_security_group" "allow_all" {
   }
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "ec2" {
   ami           = "ami-00e801948462f718a"
   instance_type = "t3.micro"
 
